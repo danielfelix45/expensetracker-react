2cc88573cbbf4272074fd6404257e847a3ce8e21
Daniel Felix <felixdaniel9@gmail.com>
2021-10-26 14:03:13 -0300

    Projeto lista de despesas com react e typescript

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+  { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.15 },
+  { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
+  { date: new Date(2021, 9, 16), category: 'rent', title: 'Casa', value: 850 },
+  { date: new Date(2021, 10, 15), category: 'salary', title: 'Barbearia Felix', value: 4800 },
+]
